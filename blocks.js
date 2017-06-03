f0e87050059766834c97009db8ec44c6becac4f5
badarovca <badaro_sacra@hotmail.com>
2017-06-02 22:51:34 -0300

    [+] Melhorando interface

--- blocks.js
+++ blocks.js
@@ -2,20 +2,6 @@ Blockly.Blocks['page_settings'] = {
   init: function() {
     this.setColour(120);
     this.appendStatementInput("page_header")
-/*    
-    this.appendStatementInput("page_header")
-        
-    this.appendValueInput("page_color")
-        .setCheck("color")
-        .appendField("Text Color:");
-    this.appendValueInput("page_background")
-        .setCheck("color")
-        .appendField("Background Color:");
-    this.appendValueInput("page_paragraph")
-        .setCheck("String")
-        .appendField("Paragraph:");
-    this.setTooltip('');
-*/
   }
 };
 
@@ -24,7 +10,7 @@ Blockly.Blocks['sobe'] = {
     this.appendValueInput("sobe")
         .setCheck("desloca")
         .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField(new Blockly.FieldImage("http://files.softicons.com/download/web-icons/web-pink-buttons-by-axialis-team/png/32/Arrow1%20Up.png", 32, 32, "*"));
+        .appendField(new Blockly.FieldImage("img/arrowUp.png", 16, 16, "*"));
     this.setPreviousStatement(true, "desloca");
     this.setNextStatement(true, "desloca");
     this.setColour(230);
@@ -37,7 +23,7 @@ Blockly.Blocks['desce'] = {
   init: function() {
     this.appendValueInput("desce")
         .setCheck("desloca")
-        .appendField(new Blockly.FieldImage("http://files.softicons.com/download/web-icons/web-pink-buttons-by-axialis-team/png/32/Arrow1%20Down.png", 32, 32, "*"));
+        .appendField(new Blockly.FieldImage("img/arrowDown.png", 16, 16, "*"));
     this.setPreviousStatement(true, "desloca");
     this.setNextStatement(true, "desloca");
     this.setColour(230);
@@ -50,7 +36,7 @@ Blockly.Blocks['esquerda'] = {
   init: function() {
     this.appendValueInput("esquerda")
         .setCheck("desloca")
-        .appendField(new Blockly.FieldImage("http://files.softicons.com/download/web-icons/web-pink-buttons-by-axialis-team/png/32/Arrow1%20Left.png", 32, 32, "*"));
+        .appendField(new Blockly.FieldImage("img/arrowLeft.png", 16, 16, "*"));
     this.setPreviousStatement(true, "desloca");
     this.setNextStatement(true, "desloca");
     this.setColour(230);
@@ -63,7 +49,7 @@ Blockly.Blocks['direita'] = {
   init: function() {
     this.appendValueInput("direita")
         .setCheck("desloca")
-        .appendField(new Blockly.FieldImage("http://files.softicons.com/download/web-icons/web-pink-buttons-by-axialis-team/png/32/Arrow1%20Right.png", 32, 32, "*"));
+        .appendField(new Blockly.FieldImage("img/arrowRight.png", 16, 16, "*"));
     this.setPreviousStatement(true, "desloca");
     this.setNextStatement(true, "desloca");
     this.setColour(230);
